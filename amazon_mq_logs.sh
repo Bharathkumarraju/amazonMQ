87e5e88f3ed0a1209d4062ff4ccd0196501382da
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-07-28 06:16:41 +0800

    add resource based policy to send logs to cloudwatch

--- amazon_mq_logs.sh
+++ amazon_mq_logs.sh
@@ -1,1 +1,18 @@
 aws --region us-west-1 logs put-resource-policy --policy-name AmazonMQ-Logs --policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"mq.amazonaws.com"},"Action":["logs:PutLogEvents","logs:CreateLogStream"],"Resource":"arn:aws:logs:*:*:log-group:/aws/amazonmq/*"}]}'
+
+
+
+
+aws --region ap-southeast-1 logs put-resource-policy --policy-name AmazonMQ-Logs-staging --policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"mq.amazonaws.com"},"Action":["logs:PutLogEvents","logs:CreateLogStream"],"Resource":"arn:aws:logs:*:*:log-group:/aws/amazonmq/*"}]}'
+
+
+bharathkumardasaraju@external$ aws-vault exec aws-staging -- aws --region ap-southeast-1 logs put-resource-policy --policy-name AmazonMQ-Logs-staging --policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"mq.amazonaws.com"},"Action":["logs:PutLogEvents","logs:CreateLogStream"],"Resource":"arn:aws:logs:*:*:log-group:/aws/amazonmq/*"}]}'
+
+{
+    "resourcePolicy": {
+        "policyName": "AmazonMQ-Logs-staging",
+        "policyDocument": "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"mq.amazonaws.com\"},\"Action\":[\"logs:PutLogEvents\",\"logs:CreateLogStream\"],\"Resource\":\"arn:aws:logs:*:*:log-group:/aws/amazonmq/*\"}]}",
+        "lastUpdatedTime": 1722118213743
+    }
+}
+bharathkumardasaraju@external$
